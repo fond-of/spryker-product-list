b98bf05a7ea2640bfea781d7a312b53e31c215d6
Daniel Rose <daniel-rose@gmx.de>
2018-09-14 14:43:47 +0200

    fix doc block

--- src/FondOfSpryker/Zed/ProductList/Business/ProductList/ProductListReader.php
+++ src/FondOfSpryker/Zed/ProductList/Business/ProductList/ProductListReader.php
@@ -19,7 +19,7 @@ class ProductListReader extends BaseProductListReader
      * @param \Spryker\Zed\ProductList\Persistence\ProductListRepositoryInterface $productListRepository
      * @param \Spryker\Zed\ProductList\Business\ProductListCategoryRelation\ProductListCategoryRelationReaderInterface $productListCategoryRelationReader
      * @param \Spryker\Zed\ProductList\Business\ProductListProductConcreteRelation\ProductListProductConcreteRelationReaderInterface $productListProductConcreteRelationReader
-     * @param \FondOfSpryker\Zed\ProductList\Business\ProductList\ProductListTransferExpanderInterface
+     * @param \FondOfSpryker\Zed\ProductList\Business\ProductList\ProductListTransferExpanderInterface $productListTransferExpander
      */
     public function __construct(
         ProductListRepositoryInterface $productListRepository,
