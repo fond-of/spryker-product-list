18274f6e48dcaf31206bced8c89e7aca75515cb5
Daniel Rose <daniel-rose@gmx.de>
2020-02-06 16:45:46 +0100

    don't extend plugin interface from business interface

--- src/FondOfSpryker/Zed/ProductList/Dependency/Plugin/ProductListPostSaverPluginInterface.php
+++ src/FondOfSpryker/Zed/ProductList/Dependency/Plugin/ProductListPostSaverPluginInterface.php
@@ -2,8 +2,19 @@
 
 namespace FondOfSpryker\Zed\ProductList\Dependency\Plugin;
 
-use Spryker\Zed\ProductList\Business\ProductList\ProductListPostSaverInterface;
+use Generated\Shared\Transfer\ProductListTransfer;
 
-interface ProductListPostSaverPluginInterface extends ProductListPostSaverInterface
+interface ProductListPostSaverPluginInterface
 {
+    /**
+     * Specification:
+     * - Plugin is triggered after product list object is saved.
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\ProductListTransfer $productListTransfer
+     *
+     * @return \Generated\Shared\Transfer\ProductListTransfer
+     */
+    public function postSave(ProductListTransfer $productListTransfer): ProductListTransfer;
 }
--- src/FondOfSpryker/Zed/ProductList/Dependency/Plugin/ProductListPreDeleterPluginInterface.php
+++ src/FondOfSpryker/Zed/ProductList/Dependency/Plugin/ProductListPreDeleterPluginInterface.php
@@ -2,8 +2,19 @@
 
 namespace FondOfSpryker\Zed\ProductList\Dependency\Plugin;
 
-use FondOfSpryker\Zed\ProductList\Business\ProductList\ProductListPreDeleterInterface;
+use Generated\Shared\Transfer\ProductListTransfer;
 
-interface ProductListPreDeleterPluginInterface extends ProductListPreDeleterInterface
+interface ProductListPreDeleterPluginInterface
 {
+    /**
+     * Specification:
+     * - Plugin is triggered before product list object is deleted.
+     *
+     * @api
+     *
+     * @param \Generated\Shared\Transfer\ProductListTransfer $productListTransfer
+     *
+     * @return void
+     */
+    public function preDelete(ProductListTransfer $productListTransfer): void;
 }
